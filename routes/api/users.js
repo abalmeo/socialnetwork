53af18761838e9eb26b15be71c025f9f65f5969c
Alvin Balmeo <albalmeo@gmail.com>
2019-05-22 01:20:03 -0700

    Adding json web tokens for user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
 
-//route      GET @api/users
-//@desc      Test route
+//route      POST @api/users
 //@access    Public
 router.post(
   "/",
@@ -43,7 +44,7 @@ router.post(
         r: "pg",
         d: "mm"
       });
-
+      //Create user
       user = new User({
         name,
         email,
@@ -54,11 +55,26 @@ router.post(
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
-
+      //save user in database
       await user.save();
 
-      //Return jsonwebtoken
-      res.send("User registered");
+      //get payload
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      //sign token
+      //pass in payoad, secret, expiration, and if no error send token back to client
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
