58064a22e6ba40ccd4e1e03d9465017d3121b5ea
Alvin Balmeo <albalmeo@gmail.com>
2019-06-10 23:56:54 -0700

    updating alert.js layout

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,12 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const Alert = ({ alerts }) => {
-  return <div />;
-  import { connect } from "react-redux";
-};
+const Alert = ({ alerts }) =>
+  (alerts !== null) & (alerts.length > 0) &&
+  alerts.map(alert => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {" "}
+      {alerts.msg}
+    </div>
+  ));
 
-Alert.propTypes = {};
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired
+};
 
 const mapStateToProps = state => ({
   alerts: state.alert
