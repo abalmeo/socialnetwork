d9e7b8a485e76e740345aee6e6d9a5c64a7ef1fa
Alvin Balmeo <albalmeo@gmail.com>
2019-06-19 22:41:07 -0700

    adding functionality for creating profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,43 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+//Create of update profile
+//history object has method called push to redirect
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    //adds or updates profile
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+    //If edit is true then profile updated otherwise profile created
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"));
+
+    //if not editing, creating profile, then rerouting to dashboard
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
