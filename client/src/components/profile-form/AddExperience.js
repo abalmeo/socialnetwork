4bb7c96bc2a492d5542cb9e6c5dbd735d47fb3f4
Alvin Balmeo <albalmeo@gmail.com>
2019-07-02 23:21:25 -0700

    adding AddExperience component

---
+++ client/src/components/profile-form/AddExperience.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addExperience } from "../../actions/profile";
+
+const AddExperience = props => {
+  return <div />;
+};
+
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addExperience }
+)(AddExperience);
