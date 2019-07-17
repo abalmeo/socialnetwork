559370914b9b4005b7ecd4fd0dd92af6aba6a4f4
Alvin Balmeo <albalmeo@gmail.com>
2019-07-17 00:44:26 -0700

    fixing model for company and updating education

--- models/Profile.js
+++ models/Profile.js
@@ -30,6 +30,10 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true
       },
+      company: {
+        type: String,
+        required: true
+      },
       location: {
         type: String
       },
