4aa695d09446a57c70126dca8dcaa357e479d1d0
Alvin Balmeo <albalmeo@gmail.com>
2019-07-29 23:56:47 -0700

    adding profile component

---
+++ client/src/components/profiles/Profile.js
@@ -0,0 +1,26 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux ";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profile = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  });
+  return <div />;
+};
+
+Profile.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const apStateToProps = (state = {
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profile);
