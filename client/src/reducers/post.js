4d3f1fbce97071ff59b68d7796f3908fefe7fb66
Alvin Balmeo <albalmeo@gmail.com>
2019-09-12 23:52:55 -0700

    Adding functionality to like and unlike posts and delete posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST
+} from "../actions/types";
 import { defaultCipherList } from "constants";
 
 const initialState = {
@@ -18,10 +23,25 @@ export default function(state = initialState, action) {
         posts: payload,
         loading: false
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
+        loading: false
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        // map through post and check if correct post, return new state with likes returnes
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false
       };
     default:
