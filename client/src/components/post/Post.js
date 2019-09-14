11ab3d05fd21f2f8789fa615fcab15db13f52f95
Alvin Balmeo <albalmeo@gmail.com>
2019-09-14 02:33:52 -0700

    Adding COmmentForm component and actions and reducers

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "../post/CommentForm";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -14,7 +16,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <Spinner />
   ) : (
     <Fragment>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
       <PostItem post={post} showActions={false}></PostItem>
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
